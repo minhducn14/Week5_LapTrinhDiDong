7bbef0fe92cb34c258023146cbd495e1e56d6f6a
unknown <vunguyenminhduc1410@gmail.com>
2024-10-02 20:16:09 +0700

    Hoan Thanh Screen 1

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import ScreenOne from './ScreenOne';
 export default function App() {
+  const Stack = createNativeStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="ScreenOne"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="ScreenOne" component={ScreenOne} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
